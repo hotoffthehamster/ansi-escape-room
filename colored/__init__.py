aec7998e7f6e2675dbfc0f8165ba73c3ecc8b899
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2015-06-06 04:50:45 +0300

    Fix import

--- colored/__init__.py
+++ colored/__init__.py
@@ -1,9 +1,9 @@
 from __future__ import print_function
 
-from .colored import *
-from .fore import fore
-from .back import back
-from .style import style
+from colored import *
+from fore import *
+from back import *
+from style import *
 
 __version_info__ = (1, 2, 0)
 __version__ = '{0}.{1}.{2}'.format(*__version_info__)
